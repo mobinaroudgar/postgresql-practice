71e984df7bab5638f60ab4374475d9eaa51e7f8a
Mobina Roudgar <roudgarmobina@gamil.com>
2026-08-30 19:58:47 +0330

    Add database schemas for GreenCycle and Company X

---
+++ database-schemas/company-x-schema.sql
@@ -0,0 +1,31 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict W03iqmAL0ahevOXGkWXcJwHcBZJ1ooMxXCmcTcG9ycLQYS092aOAQMEDPstSbfL
+
+-- Dumped from database version 17.7
+-- Dumped by pg_dump version 17.7
+
+-- Started on 2026-08-30 19:57:52
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+-- Completed on 2026-08-30 19:57:53
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict W03iqmAL0ahevOXGkWXcJwHcBZJ1ooMxXCmcTcG9ycLQYS092aOAQMEDPstSbfL
+

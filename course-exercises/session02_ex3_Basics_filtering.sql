54d8bdc9e2e9f45b2629f36e3f95e0911b410700
Mobina Roudgar <roudgarmobina@gamil.com>
2026-08-31 23:48:09 +0330

    Add session02 ex3- basics filtering

---
+++ course-exercises/session02_ex3_Basics_filtering.sql
@@ -0,0 +1,8 @@
+--3. How many payments are there where the amount is either 0
+--or is between 3.99 and 7.99 and in the same time has
+--happened on 2020-05-01.
+
+Select  count(*)
+From payment 
+where  (amount=0 or amount between 3.99 and 7.99) 
+and  (payment_date  between '2020-05-01' and '2020-05-02') 

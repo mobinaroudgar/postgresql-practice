0c1c9fe705d05bc9a0c08207f4024a2855e43c75
Mobina Roudgar <roudgarmobina@gamil.com>
2026-08-31 23:17:32 +0330

    Add session02 ex1 - basics filtering

---
+++ course-exercises/session02_ex1_Basics_filtering.sql
@@ -0,0 +1,7 @@
+--1. How many movies are there that contain 'Saga' in the description and where 
+--the title starts either with 'A' or ends with 'R'? Use the alias 'no_of_movies'.
+
+Select count(*) as no_of_movies
+From film 
+Where (description ilike '%saga%') And
+      (title ilike 'A%' or title ilike '%R')

952efd9f9bcbf1fa65f7ab2a202ba6b719505ec1
Mobina Roudgar <roudgarmobina@gamil.com>
2026-09-02 16:35:47 +0330

    Add session03 ex1 - basics Grouping, remove stray file

---
+++ course-exercises/session03_ex1_Basics_Grouping.sql
@@ -0,0 +1,8 @@
+--There are two competitions between the two employees. Which employee had the highest sales amount in a single day? Which employee had the most sales in a single day (not counting payments with amount = 0?
+
+Select  staff_id, Date(payment_date), Sum(amount), Count(*)
+From payment
+where amount!=0
+Group by staff_id, Date(payment_date)
+order by Sum(amount) desc
+

91cd8d660394a846302015d50213d6e083e84480
Mobina <150528162+mobinaroudgar@users.noreply.github.com>
2026-09-02 19:07:32 +0330

    Update session03_ex2_Basics_Grouping.sql

--- course-exercises/session03_ex2_Basics_Grouping.sql
+++ course-exercises/session03_ex2_Basics_Grouping.sql
@@ -1,4 +1,5 @@
---In 2020, April 28, 29 and 30 were days with very high revenue. That's why we want to focus in this task only on these days(filter accordingly). Find out what is the average payment amount grouped by customer and day - consider only the days/customers with more than 1 payment (per customer and day).
+--In 2020, April 28, 29 and 30 were days with very high revenue. That's why we want to focus in this task only on these days(filter accordingly). 
+--Find out what is the average payment amount grouped by customer and day - consider only the days/customers with more than 1 payment (per customer and day).
 
 Select customer_id, Date(payment_date), Round(Avg(amount),2), count(*)
 From payment 

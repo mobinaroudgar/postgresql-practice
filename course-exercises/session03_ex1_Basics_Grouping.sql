97c5f39c1e7950e81a1a223303cf43898d95f62b
mobinaroudgar <150528162+mobinaroudgar@users.noreply.github.com>
2026-09-02 16:40:07 +0330

    Update session03_ex1_Basics_Grouping.sql

--- course-exercises/session03_ex1_Basics_Grouping.sql
+++ course-exercises/session03_ex1_Basics_Grouping.sql
@@ -1,4 +1,5 @@
---There are two competitions between the two employees. Which employee had the highest sales amount in a single day? Which employee had the most sales in a single day (not counting payments with amount = 0?
+--There are two competitions between the two employees. Which employee had the highest sales amount in a single day? 
+--Which employee had the most sales in a single day (not counting payments with amount = 0?
 
 Select  staff_id, Date(payment_date), Sum(amount), Count(*)
 From payment

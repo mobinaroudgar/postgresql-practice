5e1f27f6da0c409ab0bcd8190f4296a3bdb552ef
Mobina Roudgar <roudgarmobina@gamil.com>
2026-09-10 14:14:59 +0330

    Remove session04_ex2 to re-add cleanly

--- course-exercises/session04_ex2_Intermediate_Functions.sql
+++
@@ -1,6 +0,0 @@
---Extract the last 5 characters of the email address first. 
---The email address always ends with '.org'.
---How can you extract just the dot '.' from the email address?
-
-select email, right(email,5),left(right(email,4),1)
-from customer 

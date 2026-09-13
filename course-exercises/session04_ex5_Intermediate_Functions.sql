38a7e5c79f49bfa065771233214788251fd05a92
Mobina <150528162+mobinaroudgar@users.noreply.github.com>
2026-09-13 17:48:20 +0330

    Update session04_ex5_Intermediate_Functions.sql

--- course-exercises/session04_ex5_Intermediate_Functions.sql
+++ course-exercises/session04_ex5_Intermediate_Functions.sql
@@ -1,22 +1,19 @@
 
---You need to create an anonymized form of the email addresses
---in the following way:
+--You need to create an anonymized form of the email addresses in the following way:
 --M ***. S *** @sakilacustomer.org
 --P ***. J *** @sakilacustomer.org
 
-Select email, Left(email,1)||'***.'||
-Substring (email From Position('.' In email)+1 For 1)||'***'||
-Substring(email From position('@' In email)) As new_email
+Select email,
+Left(email,1)||'***.'|| Substring (email From Position('.' In email)+1 For 1)||'***'|| Substring(email From position('@' In email)) As new_email
 From customer
 
---In a second query create an anonymized form of the email
---addresses in the following way:
+--In a second query create an anonymized form of the email addresses in the following way:
 --*** Y.S *** @sakilacustomer.org
 --*** A.J *** @sakilacustomer.org
 
-Select email, '***'|| Substring(email From Position('.' In email)-1 For 1) ||'.***'
-||Substring(email From Position('@' In email)-1 For 1)||
-substring(email from position('@' in email)) as new_email
+Select email, 
+'***'|| Substring(email From Position('.' In email)-1 For 1) ||'.***' ||Substring(email From Position('@' In email)-1 For 1)|| 
+  substring(email from position('@' in email)) as new_email
 From customer
 
 
